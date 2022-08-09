9d3645d4cb58f15dbc34444f3350a0c49d7e074f
Patel Krupa <patelkrupa209@gmail.com>
2022-08-09 12:43:05 +1000

    Initial Commit

---
+++ Swiftui-chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui-chat
+//
+//  Created by Krupa Patel on 9/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftui-chat/Swiftui_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_chatApp.swift
+//  Swiftui-chat
+//
+//  Created by Krupa Patel on 9/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
