da680d689934d28ffc06290850ad43988a67fb88
Patel Krupa <patelkrupa209@gmail.com>
2022-08-09 13:26:51 +1000

    add image ,color and fonts

---
+++ Swiftui-chat/Fonts/Fonts.swift
@@ -0,0 +1,53 @@
+//
+//  Fonts.swift
+//  Swiftui-chat
+//
+//  Created by Krupa Patel on 9/8/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    public static var body: Font {
+        
+        return Font.custom("LexendDeca-Regualr", size: 14)
+    }
+    
+    public static var button: Font {
+        
+        return Font.custom("LexendDeca-SemiBold", size: 14)
+    }
+    
+    public static var caption: Font {
+        
+        return Font.custom("LexendDeca-Regualr", size: 10)
+    }
+    
+    public static var tabBar: Font {
+        
+        return Font.custom("LexendDeca-Regualr", size: 12)
+    }
+    
+    public static var settings: Font {
+        
+        return Font.custom("LexendDeca-Regualr", size: 16)
+    }
+    
+    public static var title: Font {
+        
+        return Font.custom("LexendDeca-Bold", size: 23)
+    }
+    
+    public static var pageTitle: Font {
+        
+        return Font.custom("LexendDeca-SemiBold", size: 33)
+    }
+    
+    public static var chatHeading: Font {
+        
+        return Font.custom("LexendDeca-SemiBold", size: 19)
+    }
+  
+}
